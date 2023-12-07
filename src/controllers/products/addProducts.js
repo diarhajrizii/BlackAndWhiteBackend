b84815a455f090b7e35a7d0baa1b8421df83f86d
diarhajrizi <87374424+DiarHajrizi@users.noreply.github.com>
2023-12-07 07:15:21 +0100

    Check quantity of product

--- src/controllers/products/addProducts.js
+++ src/controllers/products/addProducts.js
@@ -9,33 +9,29 @@ module.exports = async function addProducts(req, res) {
     const queries = [];
     const values = [];
 
+    let productsQuantity = 0;
     for (const sizeObj of sizes) {
       const { size, quantity } = sizeObj;
-
+      productsQuantity += quantity;
       for (let i = 0; i < quantity; i++) {
         const insertProductQuery = `
-          INSERT INTO products (code, name, brand, type, color, price, import_price, number)
-          VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+          INSERT INTO products (code, brand_id, type_id, color_id, price, import_price, number)
+          VALUES (?, ?, ?, ?, ?, ?, ?)
         `;
         queries.push(insertProductQuery);
-        values.push([
-          code,
-          "TestProduct",
-          brand,
-          type,
-          color,
-          stockPrice,
-          importPrice,
-          size,
-        ]);
+        values.push([code, brand, type, color, stockPrice, importPrice, size]);
       }
     }
+
+    if (!productsQuantity)
+      throw { message: "Please select quantity of numbers" };
 
     const result = await executeMultiQuery({
       queries,
       values,
       dbType: dbMain, // Replace with your actual database type
     });
+
     return successfulReturn({ data: addProductsModel({ data: result }) }, res);
   } catch (error) {
     console.error(error);
