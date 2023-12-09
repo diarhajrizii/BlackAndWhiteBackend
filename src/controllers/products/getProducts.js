94008f6c13720b0d6a44b6eb05f5d87f169faa51
diarhajrizi <87374424+DiarHajrizi@users.noreply.github.com>
2023-12-09 04:41:22 +0100

    Created getProduct Function (Endpoint)

---
+++ src/controllers/products/getProducts.js
@@ -0,0 +1,26 @@
+const { query } = require("../../services/db.service");
+const { successfulReturn, errorReturn } = require("../../utils/response");
+
+module.exports = async function getProducts(req, res) {
+  try {
+    const sql = `
+        SELECT      P.*,
+                    C.name AS color, 
+                    PT.name AS type,
+                    B.name AS brand,
+                    B.produced
+        FROM        products P
+        LEFT JOIN colors C
+        ON P.color_id = C.id
+        LEFT JOIN product_type PT
+        ON P.type_id = PT.id
+        LEFT JOIN brands B
+        ON P.brand_id = B.id
+    `;
+    const products = await query({ sql, params: [], connection: dbMain });
+    return successfulReturn({ data: products }, res);
+  } catch (error) {
+    console.error(error);
+    return errorReturn({ e: error, res });
+  }
+};
--- src/routes/products/products.routes.js
+++ src/routes/products/products.routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const addProducts = require("../../controllers/products/addProducts");
+const getProducts = require("../../controllers/products/getProducts");
 router.post("/products", addProducts);
+router.get("/products", getProducts);
 
 module.exports = router;
