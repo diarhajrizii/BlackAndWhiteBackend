63765bbd249680c9900a8d8e5fee6d83d47fee32
diarhajrizi <87374424+DiarHajrizi@users.noreply.github.com>
2023-12-14 03:02:19 +0100

    Added html to emailSender function

--- src/controllers/emailSender.js
+++ src/controllers/emailSender.js
@@ -22,6 +22,68 @@ const sendEmail = (to, subject, text) => {
     to,
     subject,
     text,
+    html: `
+    <html>
+    <head>
+      <style>
+        /* Mobile styles */
+        @media only screen and (max-width: 600px) {
+          .container {
+            width: 100%;
+            padding: 10px;
+            text-align: center;
+          }
+          img {
+            height: 90px !important;
+            width: 90px !important;
+          }
+        }
+        body {
+          font-family: Arial, sans-serif;
+          background-color: #f4f4f4;
+          color: #333;
+          text-align: center;
+        }
+        .container {
+          max-width: 600px;
+          border: 30px solid black;
+          width: 80%;
+          margin: 0 auto;
+          padding: 20px;
+          background-color: #fff;
+          border-radius: 5px;
+          box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+          text-align: center;
+        }
+        img {
+          height: 170px;
+          width: 170px;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="container">
+        <div class="img-container">
+          <img
+            src="https://i.pinimg.com/564x/f2/c3/cd/f2c3cd120df823e9de2c49c8466fbc26.jpg"
+          />
+        </div>
+        <h1>Hello MR. Hajrizi!</h1>
+        <p>
+          You have an outstanding rent payment for your store, which is due by
+          <strong>05.12.2023</strong>. Please ensure the payment is made by the
+          deadline to avoid any inconvenience.
+        </p>
+        <!-- <p>
+          You can add more content, images, links, and apply styles using HTML and
+          CSS.
+        </p> -->
+      </div>
+    </body>
+  </html>
+  
+  
+  `,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
