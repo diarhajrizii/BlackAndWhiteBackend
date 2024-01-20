0d9186c4c6e263679f02f543b898506abfc9f0ce
diarhajrizi <87374424+DiarHajrizi@users.noreply.github.com>
2024-01-20 15:44:34 +0100

    Added Accessories module at addProduct Function and updated getProduct Function

--- src/controllers/products/addProducts.js
+++ src/controllers/products/addProducts.js
@@ -13,33 +13,61 @@ module.exports = async function addProducts(req, res) {
       importPrice,
       sizes,
       productType,
+      name, // New property for accessories
+      quantity, // New property for accessories
     } = req.body;
 
     const queries = [];
     const values = [];
     let productsQuantity = 0;
 
-    for (const sizeObj of sizes) {
-      const { size, quantity } = sizeObj;
+    if (productType === "accessories") {
+      // Insert accessories with a single size
+      const barcode = `${code}-${brand}-${name}`;
+      const insertProductQuery = `
+        INSERT INTO products (code, brand_id, type_id, color_id, price, import_price, number, barcode, date, type, location_id, name, quantity)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?, 1, ?, ?)
+      `;
+      queries.push(insertProductQuery);
+      values.push([
+        code,
+        brand,
+        type,
+        color,
+        stockPrice,
+        importPrice,
+        0, // Single size for accessories
+        barcode,
+        productType,
+        name,
+        quantity,
+      ]);
+
       productsQuantity += quantity;
-      for (let i = 0; i < quantity; i++) {
-        const barcode = `${code}-${brand}-${size}-${color}`;
-        const insertProductQuery = `
-          INSERT INTO products (code, brand_id, type_id, color_id, price, import_price, number, barcode, date, type)
-          VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?)
-        `;
-        queries.push(insertProductQuery);
-        values.push([
-          code,
-          brand,
-          type,
-          color,
-          stockPrice,
-          importPrice,
-          size,
-          barcode,
-          productType,
-        ]);
+    } else {
+      // Insert sizes for shoes and textile
+      for (const sizeObj of sizes) {
+        const { size, quantity } = sizeObj;
+        productsQuantity += quantity;
+        for (let i = 0; i < quantity; i++) {
+          const barcode = `${code}-${brand}-${size}-${color}`;
+          const insertProductQuery = `
+            INSERT INTO products (code, brand_id, type_id, color_id, price, import_price, number, barcode, date, type, location_id)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?, 1)
+          `;
+          queries.push(insertProductQuery);
+          values.push([
+            code,
+            brand,
+            type,
+            color,
+            stockPrice,
+            importPrice,
+            size,
+            barcode,
+            productType,
+          ]);
+        }
       }
     }
 
--- src/controllers/products/getProducts.js
+++ src/controllers/products/getProducts.js
@@ -4,18 +4,38 @@ const { successfulReturn, errorReturn } = require("../../utils/response");
 module.exports = async function getProducts(req, res) {
   try {
     const sql = `
-        SELECT      P.*,
-                    C.name AS color, 
-                    PT.name AS type,
-                    B.name AS brand,
-                    B.produced
-        FROM        products P
-        LEFT JOIN colors C
-        ON P.color_id = C.id
-        LEFT JOIN product_specific_types PT
-        ON P.type_id = PT.id
-        LEFT JOIN brands B
-        ON P.brand_id = B.id
+      SELECT
+          P.id,
+          P.barcode,
+          P.code,
+          P.brand_id,
+          P.type_id,
+          P.color_id,
+          P.price,
+          P.import_price,
+          P.number,
+          P.date,
+          P.type,
+          P.location_id,
+          COUNT(P.barcode) AS quantity,
+          C.name AS color,
+          PT.name AS type,
+          B.name AS brand,
+          B.produced,
+          L.name AS location
+      FROM
+          products P
+      LEFT JOIN
+          colors C ON P.color_id = C.id
+      LEFT JOIN
+          product_specific_types PT ON P.type_id = PT.id
+      LEFT JOIN
+          brands B ON P.brand_id = B.id
+      LEFT JOIN 
+          locations L ON P.location_id = L.id
+      GROUP BY
+          P.barcode;
+
     `;
     const products = await query({ sql, params: [], connection: dbMain });
     return successfulReturn({ data: products }, res);
